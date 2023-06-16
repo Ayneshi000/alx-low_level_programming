6ce54a6fd8c68c54738bb7ea16ec229ee9c062a3
nermeen <you@example.com>
2023-06-16 15:59:13 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,3 @@
-
-/*
- * File: 9-print_comb.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
@@ -14,12 +8,11 @@
 int main(void)
 {
 	int num;
-for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
