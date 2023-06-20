6259dda410cf26a3714572174669d1d84c871d85
nermeen <you@example.com>
2023-06-20 13:48:04 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,13 @@
  */
 int main(void)
 {
+	char str[] = "_putchar";
+	int count_str;
 
-   char str[] = "_putchar";
-   int count_str;
-
-   for (count_str = 0; count_str<8; count_str ++)
-   {
-                 _putchar(str[count_str]);
-   
-   }	 
-		 _putchar("\n")  
-  return (0);
+	for (count_str = 0; count_str < 8; count_str++)
+	{
+		_putchar(str[count_str]);
+	}
+	_putchar('\n');
+	return (0);
 }
