6e8958f02fef653cd50bd58464a2e3b5e795aa99
nermeen <you@example.com>
2023-06-20 13:04:46 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(){
+
+   char str[]= "_putchar";
+   int count_str;
+     for (count_str = 0; count_str<8; count_str ++)
+                 _putchar(str[count_str]);
+     _putchar("\n")  
+
+  return (0);
+}
