d8be3687a4270610b50a23fd03266fe50523ad1b
nermeen <you@example.com>
2023-06-20 14:42:13 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10() print the alphbets in 10 time
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for(i = 0; i < 10; i++)
+	{
+		for(c = 'a'; i <= 'z'; c++)
+		{ 
+			_putchar(c);
+		}
+	}
+
+}
