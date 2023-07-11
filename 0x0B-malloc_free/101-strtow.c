c63d7c6b011ab55b22e48ade45f9b42e1e674c8a
nermeen <you@example.com>
2023-07-11 15:40:55 -0700

    add

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 
 /**
- * strtow - Concatenates all the arguments of your program
- * @str: String to process
- *
- * Return: Pointer to a new string
+ * strtow - concatenates all the arguments of your program
+ *@str: string
+ *@av: arguments
+ * Return: a pointer to a new string
  */
 char **strtow(char *str)
 {
@@ -18,32 +18,27 @@ char **strtow(char *str)
 	j = 0;
 	i = 0;
 	count = 0;
-
 	if (*str == '\0' || str == NULL)
 		return (NULL);
-
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
 			w++;
 	}
-
 	p = (char **)malloc((w + 1) * sizeof(char *));
 	if (p == NULL)
 		return (NULL);
-
 	for (wordf = 0; str[wordf] && j <= w; wordf++)
 	{
 		count = 0;
 		if (str[wordf] != ' ')
 		{
-			for (i = wordf; str[i] != '\0'; i++)
+			for (i = wordf ; str[i] != '\0'; i++)
 			{
 				if (str[i] == ' ')
 					break;
 				count++;
 			}
-
 			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
 			if (*(p + j) == NULL)
 			{
@@ -55,18 +50,15 @@ char **strtow(char *str)
 				free(p);
 				return (NULL);
 			}
-
 			for (m = 0; wordf < i; wordf++)
 			{
 				p[j][m] = str[wordf];
 				m++;
 			}
-
 			p[j][m] = '\0';
 			j++;
 		}
 	}
-
 	p[j] = NULL;
 	return (p);
 }
